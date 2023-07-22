aa9243dd79ce3976479b536e5e3c7923f00959d9
srirajvj <srirajvj77@gmail.com>
2023-07-22 11:38:07 +0530

    code saved

---
+++ main.py
@@ -0,0 +1,5 @@
+
+print("Welcome to the band name generator")
+x=input("What is the name of the city you grew up in:\n")
+y=input("What is the name of your pet:\n")
+print("The name of your band is "+x+" "+y)
